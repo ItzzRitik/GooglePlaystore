a3818173f5a3d0d76dc09c372d94984f1662216a
Ritik Srivastava <ritik.space@gmail.com>
2018-10-25 00:37:15 +0530

    Developing Adapters

--- app/src/main/java/in/sanrakshak/googleplaystore/adapters/TopCategoryItemAppAdapter.java
+++ app/src/main/java/in/sanrakshak/googleplaystore/adapters/TopCategoryItemAppAdapter.java
@@ -8,8 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.pushpal.googleplayclone.R;
-import com.pushpal.googleplayclone.models.TopCategoryItemAppModel;
+import in.sanrakshak.googleplaystore.R;
+import in.sanrakshak.googleplaystore.models.TopCategoryItemAppModel;
 
 import java.util.ArrayList;
 
