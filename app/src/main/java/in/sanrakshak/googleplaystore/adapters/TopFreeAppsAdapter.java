3224c593b042c934d9d5daa3ba6d9bc1384bcc64
Ritik Srivastava <ritik.space@gmail.com>
2018-10-25 00:39:32 +0530

    Developing Adapters

--- app/src/main/java/in/sanrakshak/googleplaystore/adapters/TopFreeAppsAdapter.java
+++ app/src/main/java/in/sanrakshak/googleplaystore/adapters/TopFreeAppsAdapter.java
@@ -10,8 +10,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.pushpal.googleplayclone.R;
-import com.pushpal.googleplayclone.models.TopFreeAppModel;
+import in.sanrakshak.googleplaystore.R;
+import in.sanrakshak.googleplaystore.models.TopFreeAppModel;
 
 import java.util.ArrayList;
 
