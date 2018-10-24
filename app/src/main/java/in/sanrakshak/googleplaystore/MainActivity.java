301f09035411f3e5c0942b716d4431bc1bbf9719
Ritik Srivastava <ritik.space@gmail.com>
2018-10-25 00:16:03 +0530

    Developing Adapters

--- app/src/main/java/in/sanrakshak/googleplaystore/MainActivity.java
+++ app/src/main/java/in/sanrakshak/googleplaystore/MainActivity.java
@@ -1,5 +1,6 @@
 package in.sanrakshak.googleplaystore;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), "Home");
+        adapter.addFragment(new HomeFragment(), "Games");
+        adapter.addFragment(new HomeFragment(), "Movies");
+        adapter.addFragment(new HomeFragment(), "Books");
+        adapter.addFragment(new HomeFragment(), "Music");
+        viewPager.setAdapter(adapter);
     }
 }
