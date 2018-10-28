a804a60405bc56b807b53da8d62d47312c74ef69
Ritik Srivastava <ritik.space@gmail.com>
2018-10-28 22:55:40 +0530

    Data Viz Panel Animation

--- app/src/main/java/in/sanrakshak/googleplaystore/VizActivity.java
+++ app/src/main/java/in/sanrakshak/googleplaystore/VizActivity.java
@@ -3,12 +3,22 @@ package in.sanrakshak.googleplaystore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class VizActivity extends AppCompatActivity {
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        overridePendingTransition(R.anim.splash_out, R.anim.splash_in);
+        return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viz);
-        setTitle("Visualize PlayStore");
+        setTitle("Visualize Play Store");
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
