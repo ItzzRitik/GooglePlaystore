5fa91412d07f00bc869125538def4ef816dc7748
Ritik Srivastava <ritik.space@gmail.com>
2018-10-28 23:50:33 +0530

    Data Viz Panel Animation

--- app/src/main/java/in/sanrakshak/googleplaystore/VizActivity.java
+++ app/src/main/java/in/sanrakshak/googleplaystore/VizActivity.java
@@ -3,10 +3,16 @@ package in.sanrakshak.googleplaystore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class VizActivity extends AppCompatActivity {
-
+    HorizontalBarChart hbc;
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
@@ -24,5 +30,25 @@ public class VizActivity extends AppCompatActivity {
         setTitle("Visualize Store");
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        hbc = findViewById(R.id.hbc);
+
+        BarData data = new BarData(getDataSet());
+        hbc.setData(data);
+        hbc.animateXY(2000, 2000);
+        hbc.invalidate();
+    }
+    private BarDataSet getDataSet() {
+
+        ArrayList<BarEntry> entries = new ArrayList();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+
+        BarDataSet dataset = new BarDataSet(entries,"hi");
+        return dataset;
     }
 }
