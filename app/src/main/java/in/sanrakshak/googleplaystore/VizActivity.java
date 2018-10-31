2ac858ff3771986faaed6c37c82dfd2d1b09ffce
Ritik Srivastava <ritik.space@gmail.com>
2018-10-31 22:43:14 +0530

    Adding Spinner

--- app/src/main/java/in/sanrakshak/googleplaystore/VizActivity.java
+++ app/src/main/java/in/sanrakshak/googleplaystore/VizActivity.java
@@ -67,19 +67,22 @@ public class VizActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
             {
                 if(position==0){
-                    hbc0.setVisibility(View.VISIBLE);
-                    hbc1.setVisibility(View.GONE);
-                    hbc2.setVisibility(View.GONE);
+                    hbc0.setElevation(10);
+                    hbc1.setElevation(9);
+                    hbc2.setElevation(8);
+                    hbc1.animate();
                 }
                 else if(position==1){
-                    hbc0.setVisibility(View.GONE);
-                    hbc1.setVisibility(View.VISIBLE);
-                    hbc2.setVisibility(View.GONE);
+                    hbc0.setElevation(9);
+                    hbc1.setElevation(10);
+                    hbc2.setElevation(9);
+                    hbc2.animate();
                 }
                 else if(position==2){
-                    hbc0.setVisibility(View.GONE);
-                    hbc1.setVisibility(View.GONE);
-                    hbc2.setVisibility(View.VISIBLE);
+                    hbc0.setElevation(9);
+                    hbc1.setElevation(9);
+                    hbc2.setElevation(10);
+                    hbc2.animate();
                 }
             }
             public void onNothingSelected(AdapterView<?> parent){}
@@ -95,18 +98,12 @@ public class VizActivity extends AppCompatActivity {
         hbc.setVisibleXRange(1000,10);
         hbc.setVisibleYRange(6,6, YAxis.AxisDependency.LEFT);
         hbc.moveViewToX(0);
-        hbc.getXAxis().setValueFormatter(new IAxisValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                return xLabel.get((int) value);
-            }
-        });
         hbc.invalidate();
     }
     private BarDataSet getDataSet(int type) {
 
         ArrayList<BarEntry> entries = new ArrayList<>();
-        xLabel = new ArrayList<>();
+        xLabel=new ArrayList<>();
         try {
             String [] nextLine;
             int lineNumber = 0;
@@ -117,15 +114,18 @@ public class VizActivity extends AppCompatActivity {
                 Log.w("coverPic", type+"");
                 if(type==0){
                     xLabel.add(nextLine[0]);
+                    entries.add(new BarEntry(lineNumber, Float.parseFloat(nextLine[2])));
                 }
                 else if (type==1){
-                    xLabel.add(nextLine[9]);
+                    xLabel.add(nextLine[1]);
+                    entries.add(new BarEntry(lineNumber, Float.parseFloat(nextLine[2])));
                 }
                 else if (type==2){
-                    xLabel.add(nextLine[2]);
+                    xLabel.add(nextLine[9]);
+                    entries.add(new BarEntry(lineNumber, Float.parseFloat(nextLine[2])));
                 }
-                entries.add(new BarEntry(lineNumber, Float.parseFloat(nextLine[2])));
             }
+
         }
         catch (Exception e) {
             Log.w("coverPic", e.toString());
